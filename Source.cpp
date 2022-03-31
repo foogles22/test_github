4ba03c02b9fcef5adac0363bbc70975c9a7a8d28
NguyenKha03 <98521796+foogles22@users.noreply.github.com>
2022-03-31 15:11:58 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,6 @@
+﻿void changePassword(Node_student*& a) {
+	print_type_in_box(30, 10, "Type in your new password", 30);
+	string b; cin >> b;
+	a->data.password = b;
+	print_double_square(30, 15, 24, 3, 15, "New password accepted!");
+}
